a062a2d1bef73e600ccb4afb4fdfbce3678897bd
Alex <ultv@inbox.ru>
2018-05-08 19:16:19 +0300

    Create ReadIndex(), ReviseCol(), ReviseRows(), ReviseDiag(). Functional implemented.

--- Program.cs
+++ Program.cs
@@ -9,19 +9,6 @@ namespace HomeWork1
 {
     class Program
     {
-        // Взаимодействует с пользователем в консоли.
-        class User
-        {
-            protected int num = 3;
-
-            public int Num
-            {
-                get { return num; }
-                set { num = value; }
-            }
-                                 
-        }
-
         
         // Осуществляет все операции над матрицей.
         class Matrix
@@ -76,7 +63,7 @@ namespace HomeWork1
                 {
                     for (int j = 0; j < num; j++)
                     {
-                        Console.Write(massiv[i, j] + " ");
+                        Console.Write(massiv[i, j] + "\t");
                     }
                     Console.WriteLine();
                 }
@@ -95,12 +82,80 @@ namespace HomeWork1
                     }
             }
 
+            // Читает с консоли индексы вводимого элемента.
+            public int[] ReadIndex()
+            {
+                Console.WriteLine("Введите номер строки: ");
+                int i = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите номер элемента в строке: ");
+                int j = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите [" + i + "," + j + "] элемент массива");            
+                massiv[i - 1,j - 1] = Int32.Parse(Console.ReadLine());
+
+                int [] x = new int[2] { i - 1, j - 1 };
+                return x;
+            }
+
+
+            // Проверяет элементы строки на равенство первому элементу в строке.
+            public bool ReviseRows(int i)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    if (massiv[i, j] != massiv[i, 0])
+                        return false;                    
+                }
+                
+                return true;
+            }
+
+            // Проверяет элементы столбца на равенство первому элементу в столбце.
+            public bool ReviseCol(int j)
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    if (massiv[i, j] != massiv[0, j])
+                        return false;
+                }
+
+                return true;
+            }
+
+            // Проверяет элементы диагоналей матрицы на равенство первым элементам в каждой диагонали.
+            // Если индексы вводимого элемента не равны - побочная диагональ не изменялась,
+            // соответственно проверяем только главную диагональ.
+            public bool ReviseDiag(int rows, int col)
+            {
+                if (rows != col)
+                {
+                    for (int i = 0; i < num; i++)
+                    {
+                        if (massiv[i, num - (i + 1)] != massiv[0, num - 1])
+                            return false;
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < num; i++)
+                    {
+                        if (massiv[i, i] != massiv[0, 0])
+                            return false;
+                    }
+
+                }
+
+                return true;
+            }
+
         }
 
 
 
 
-
+        // Присваивает значения элементам матрицы по введенным значениям индексов.
+        // Завершает выполнение, если равны значения всех элементов строки, столбца или диагоналей.
         static void Main(string[] args)
         {
           
@@ -109,13 +164,20 @@ namespace HomeWork1
 
             Matrix mat = new Matrix(num);
             mat.Print();
-            mat.Read();
-            mat.Print();
+            //mat.Read();
 
+            int[] x = new int[2];
 
+            do
+            {
+                x = mat.ReadIndex();
 
+                mat.Print();
 
+            } while ((!mat.ReviseRows(x[0])) && (!mat.ReviseCol(x[1])) && (!mat.ReviseDiag(x[0], x[1])));
 
+            Console.WriteLine("Программа успешно завершена.");
+           
 
             Console.ReadKey();
 
