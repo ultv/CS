3c6ad3cf650a6c94aeed7e7b7dc76e9a2047ec34
Alex <ultv@inbox.ru>
2018-05-11 22:34:30 +0300

    Create class Card.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,44 @@ namespace HomeWork1
     class Program
     {
 
+        class Card
+        {
+            protected int val = 0;
+            protected bool inGame = false;
 
+            public int Val { get; set; }
+            public bool InGame { get; set; }
+
+            // Выводит карту и значение.
+            // Десять выводим как римскую.
+            public void PrintVal(int v)
+            {
+                if(v == 10)
+                {
+                    Console.WriteLine(" ___");
+                    Console.WriteLine("[   ]");
+                    Console.WriteLine("[ X ]");
+                    Console.WriteLine("[___]");
+                }
+                else
+                {
+                    Console.WriteLine(" ___");
+                    Console.WriteLine("[   ]");
+                    Console.WriteLine("[ " + v + " ]");
+                    Console.WriteLine("[___]");
+                }
+
+            }
+
+            public void PrintShirt()
+            {
+                Console.WriteLine(" ___");
+                Console.WriteLine("[   ]");
+                Console.WriteLine("[ ? ]");
+                Console.WriteLine("[___]");
+            }
+            
+        }
 
 
 
@@ -18,8 +55,14 @@ namespace HomeWork1
         static void Main(string[] args)
         {
 
+            Card card1 = new Card();
+
+            card1.PrintShirt();
+            card1.PrintVal(1);
+            card1.PrintVal(10);
 
 
+            Console.ReadKey();
         }
     }
 }
